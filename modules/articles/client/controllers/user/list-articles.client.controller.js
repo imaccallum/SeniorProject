3820ba2cec1c4747c68ff642d115a29dfb49494e
kgrossnickle <kgrossnickle@ufl.edu>
2018-01-29 15:46:39 -0500

    Tried to add search

--- modules/articles/client/controllers/user/list-articles.client.controller.js
+++ modules/articles/client/controllers/user/list-articles.client.controller.js
@@ -10,6 +10,31 @@
   function ArticlesAdminListController(ArticlesService) {
     var vm = this;
 
-    vm.articles = ArticlesService.query();
+    //vm.articles = ArticlesService.query();
+        ArticlesService.query(function (data) {
+      vm.articles = data;
+      vm.buildPager();
+    });
+
+    function buildPager() {
+      vm.pagedItems = [];
+      vm.itemsPerPage = 15;
+      vm.currentPage = 1;
+      vm.figureOutItemsToDisplay();
+    }
+
+    function figureOutItemsToDisplay() {
+      vm.filteredItems = $filter('filter')(vm.articles, {
+        $: vm.search
+      });
+      vm.filterLength = vm.filteredItems.length;
+      var begin = ((vm.currentPage - 1) * vm.itemsPerPage);
+      var end = begin + vm.itemsPerPage;
+      vm.pagedItems = vm.filteredItems.slice(begin, end);
+    }
+
+    function pageChanged() {
+      vm.figureOutItemsToDisplay();
+    }
   }
 }());
